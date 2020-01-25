82f246640010c6578f7bfb0b5576924c036598d6
errol641 <56890105+errol641@users.noreply.github.com>
2020-01-24 22:14:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-tapered-69262',
       user : 'postgres',
       password : 'errol911E',
       database : 'facerecognition'
