11dc116b97b1e62a4bde031e7bf8412b7d91bfa1
errol641 <56890105+errol641@users.noreply.github.com>
2020-01-24 21:49:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.poist('/', (req, res) => res.send('it is working!'));
+
 app.post('/signin', (req, res) => {
     signin.handleSignin(req, res, db, bcrypt);
 })
